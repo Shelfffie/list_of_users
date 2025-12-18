e6a975b965c24741e552253dceb633d9cc7734d4
Polina <zilnovapolina@gmail.com>
2025-12-18 21:10:42 +0200

    add edit view in userList.tsx

--- listOfUsers/src/components/usersList.tsx
+++ listOfUsers/src/components/usersList.tsx
@@ -3,7 +3,9 @@ import { useState, useEffect } from "react";
 import { userValues } from "./type";
 
 export default function UsersList() {
-  const [users, setUsers] = useState<any>(null);
+  const [users, setUsers] = useState<userValues[]>([]);
+  const [editedData, setEditedData] = useState<Partial<userValues>>({});
+  const [editUser, setEditUser] = useState<string | null>(null);
 
   useEffect(() => {
     const getUsers = async () => {
@@ -16,19 +18,88 @@ export default function UsersList() {
     getUsers();
   }, []);
 
+  const setEdit = (user: userValues) => {
+    setEditUser(user.id);
+    setEditedData(user);
+  };
+
+  const setEditedDataFunc = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const inputName = e.target.name as keyof userValues;
+    const value = e.target.value;
+
+    setEditedData((prev) => (prev ? { ...prev, [inputName]: value } : {}));
+  };
+
+  const saveChanges = (id: string) => {
+    setEditUser(null);
+    setUsers((prev) =>
+      prev.map((user) => (user.id === id ? { ...user, ...editedData } : user))
+    );
+  };
+
   return (
     <>
       <h1>Список користувачів:</h1>
       <ul>
-        {users?.map((user: userValues) => (
-          <li key={user.id}>
-            <p>Name: {user.name}</p>
-            <p>Last name: {user.lastName}</p>
-            <p>Email: {user.email}</p>
-            <p>Phone: {user.phone}</p>
-            <p>Birthday: {user.birthday}</p>
-          </li>
-        ))}
+        {users?.map((user: userValues) =>
+          user.id === editUser ? (
+            <li key={user.id}>
+              <p>Name:</p>
+              <input
+                type="text"
+                value={editedData?.name}
+                name="name"
+                onChange={(e) => setEditedDataFunc(e)}
+              />
+              <p>Last name</p>{" "}
+              <input
+                type="text"
+                value={editedData?.lastName}
+                name="lastName"
+                onChange={(e) => setEditedDataFunc(e)}
+              />
+              <p>Email:</p>{" "}
+              <input
+                type="text"
+                value={editedData?.email}
+                name="email"
+                onChange={(e) => setEditedDataFunc(e)}
+              />
+              <p>Phone:</p>{" "}
+              <input
+                type="text"
+                value={editedData?.phone}
+                name="phone"
+                onChange={(e) => setEditedDataFunc(e)}
+              />
+              <p>Birthday:</p>{" "}
+              <input
+                type="date"
+                value={editedData?.birthday}
+                name="birthday"
+                onChange={(e) => setEditedDataFunc(e)}
+              />
+              <button onClick={() => saveChanges(user.id)}>Save</button>
+              <button
+                onClick={() => {
+                  setEditUser(null);
+                  setEditedData({});
+                }}
+              >
+                Cancel
+              </button>
+            </li>
+          ) : (
+            <li key={user.id}>
+              <p>Name: {user.name}</p>
+              <p>Last name: {user.lastName}</p>
+              <p>Email: {user.email}</p>
+              <p>Phone: {user.phone}</p>
+              <p>Birthday: {user.birthday}</p>
+              <button onClick={() => setEdit(user)}>Edit</button>
+            </li>
+          )
+        )}
       </ul>
     </>
   );
